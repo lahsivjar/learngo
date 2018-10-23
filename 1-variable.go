793293995899ec4e0c0491d8bee72714ee2f729a
Vishal Raj <lahsivjar@gmail.com>
2018-10-23 22:47:50 +0800

    One with variables

--- 1-variable.go
+++ 1-variable.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	// Declare primitive (declaration and assignment)
+	// Declare primitive (declaration, type inference and assignment all at once)
 	str := "Hello Go~"
 
 	// fmt is standard go library package, used for printing stuff
@@ -21,4 +21,11 @@ func main() {
 	// Another way to declare and assign
 	var str3 = "Another hello"
 	fmt.Println(str3)
+
+	// Multiple declaration (NOTE: both `a` and `b` are declared as int type
+	var a, b int
+	b = 5
+	a = 10
+	fmt.Println(a)
+	fmt.Println(b)
 }
